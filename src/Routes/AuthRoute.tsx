c47168f3618bae81d8591ccf2bf060c5b984eb9b
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-11-20 13:18:41 +0100

    Update AuthRoute.tsx

--- src/Routes/AuthRoute.tsx
+++ src/Routes/AuthRoute.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../Firebase/firebase.js';
+import { auth } from '../Firebase/firebase';
 
 const AuthRoute = ({ children }) => {
     const navigate = useNavigate();
