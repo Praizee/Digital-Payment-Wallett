d8f85311b5582c929261fe4cc41a34effda14340
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-05 13:58:54 +0100

    FC

--- src/Pages/Support/Support.tsx
+++ src/Pages/Support/Support.tsx
@@ -1,6 +1,6 @@
+import { FC } from 'react'
 
-
-const Support = () => {
+const Support: FC = () => {
     return (
         <div className="container pt-24 laptop:mr-[rem]">
             <div className='bg-white space-y-4 px-4 py-8 tablet:p-6 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
