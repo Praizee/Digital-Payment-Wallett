f2a8a9190bccbb8310f4b6d01845d7d4e8f60566
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-06 12:43:25 +0100

    Update TransactionHistory.tsx

--- src/Pages/TransactionHistory/TransactionHistory.tsx
+++ src/Pages/TransactionHistory/TransactionHistory.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react'
 import { CustomerTable } from '../../Components/Main/CustomerTable'
 
-const TransactionHistory = () => {
+const TransactionHistory: FC = () => {
     return (
         <div className="container pt-24 laptop:mr-[rem]">
             <div className='bg-white space-y-4 px-4 py-8 tablet:p-6 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
